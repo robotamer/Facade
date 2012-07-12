101090281851f866823dd78487f8e1739a79326e
Dennis T Kaplan <gruessle@gmail.com>
2012-07-12 11:36:08 -0700

    init

---
+++ Facade/Singleton.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * The RoboTamer Master Singleton PHP Class
+ */
+
+/**
+ * The MIT License (MIT)
+ * 
+ * Copyright © 2012 RoboTamer http://robotamer.github.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ * THE SOFTWARE. 
+ */
+
+/**
+ * The RoboTamer Master Singleton PHP Class
+ *
+ * One singleton to call and store any class as singleton
+ *
+ * @package    RoboTaMeR
+ * @author     Dennis T Kaplan
+ * @copyright  Copyright (c) 2008 - 2010, Dennis T Kaplan
+ * @license    http://robotamer.github.com
+ * @link       http://robotamer.github.com
+ *
+ * <code>
+ * $l = Singleton::factory('Translate');
+ * $v = S::Template();
+ * S::Template()->title = 'My Singleton claas';
+ * 
+ * print_r(Singleton::getClasses());
+ * </code>
+ *
+ */
+class_alias ('Singleton', 'S');
+
+class Singleton {
+
+    protected static $instances = array ();
+
+    protected function __construct () {
+	
+    }
+
+    protected static function init () {
+
+	if (!array_key_exists ('Singleton', self::$instances)) {
+	    
+	    self::$instances['Singleton'] = new Singleton();
+	}
+    }
+
+    public static function factory ($class = NULL, $arguments = array ()) {
+
+	self::init ();
+
+	if (!array_key_exists ($class, self::$instances) && $class !== NULL) {
+	    self::$instances[$class] = new $class (implode (', ', $arguments));
+	}
+
+	return self::$instances[$class];
+    }
+
+    public static function alias($class,$alias) {
+	
+	if (!array_key_exists ($alias, self::$instances) && array_key_exists ($class, self::$instances)) {
+	    self::$instances[$alias] = &self::$instances[$class];
+	}
+    }
+
+
+    /**
+     * Use like this:	$tmpl = S::Template();
+     *
+     * @param	string $className
+     * @param	mixed $arguments Class __construct arguments
+     * @return	object
+     * */
+    public static function __callStatic ($className, $arguments= array ()) {
+	
+	return self::factory ($className, $arguments);
+    }
+
+    public static function set ($name, $object) {
+
+	if (!array_key_exists ($name, self::$instances) && is_object ($object)) {
+
+	    self::init ();
+
+	    self::$instances[$name] = $object;
+
+	    return self::$instances[$name];
+	}
+
+	return FALSE;
+    }
+
+    public static function has ($name) {
+
+	if (array_key_exists ($name, self::$instances)) {
+	    return TRUE;
+	} else {
+	    return FALSE;
+	}
+    }
+
+    public static function del ($class) {
+
+	unset (self::$instances[$class]);
+    }
+
+    public function getClasses () {
+	
+	$array = array ();
+	
+	foreach (array_keys (self::$instances, TRUE) as $class){
+	    $array[] = $class;
+	}
+	
+	return $array;
+    }
+
+    protected function __clone () {
+	// Not cloning
+    }
+
+    public function __destruct () {
+	
+	foreach (array_keys (array_reverse (self::$instances, TRUE)) as $class) {
+	    unset (self::$instances[$class]);
+	}
+    }
+}
+
+?>
--- README.md
+++ README.md
@@ -1,4 +1,59 @@
-Facade
-======
+RoboTamer Facade
+================
 
 RoboTamer Facade is a component dependency substitution system to achieve component independentcy via the facade programming pattern.
+
+
+RoboTamer Master Singleton PHP Class
+====================================
+
+Usage
+-----
+
+# Via the factory
+    <?php
+    $l = Singleton::factory('Translate');
+
+# Directs call
+    <?php
+    $view = Singleton::Template();
+
+# Class Alias
+    <?php
+    Singleton::alias('Template', 'V');
+
+# Aliased call _Singleton has an S alias by default_
+    <?php
+    $view = S::V();
+
+
+# No need for global variables
+    <?php
+    S::V()->var = 'Master Singleton Class';
+
+# Use original class or alias or switch back and forth
+    <?php
+    echo Singleton::Template()->fetch(__dir__ .'/gui/layout.php');
+
+**or as alias**
+  
+    <?php
+    echo S::V()->fetch(__dir__ .'/gui/layout.php');
+
+
+print_r(S::getClasses(), true)
+
+
+Copyright
+#########
+
+The MIT License (MIT)
+---------------------
+
+**Copyright © 2012 RoboTamer**  
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:  
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.  
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
